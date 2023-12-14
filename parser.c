a3a7a5132e0e2aacb0df878fbb3b8c5b2941ba9d
nothandosibanyoni <142825922+nothandosibanyoni@users.noreply.github.com>
2023-12-14 02:20:35 +0200

    Update parser.c

--- parser.c
+++ parser.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * is_cmd - determines if a file is an executable command
+ * is_cmd - regulates if a file is an executable command
  * @info: the info struct
  * @path: path to the file
  *
@@ -33,11 +33,11 @@ int is_cmd(info_t *info, char *path)
 char *dup_chars(char *pathstr, int start, int stop)
 {
 	static char buf[1024];
-	int i = 0, k = 0;
+	int a = 0, k = 0;
 
-	for (k = 0, i = start; i < stop; i++)
-		if (pathstr[i] != ':')
-			buf[k++] = pathstr[i];
+	for (k = 0, a = start; a < stop; a++)
+		if (pathstr[a] != ':')
+			buf[k++] = pathstr[a];
 	buf[k] = 0;
 	return (buf);
 }
@@ -52,7 +52,7 @@ char *dup_chars(char *pathstr, int start, int stop)
  */
 char *find_path(info_t *info, char *pathstr, char *cmd)
 {
-	int i = 0, curr_pos = 0;
+	int a = 0, curr_pos = 0;
 	char *path;
 
 	if (!pathstr)
@@ -64,9 +64,9 @@ char *find_path(info_t *info, char *pathstr, char *cmd)
 	}
 	while (1)
 	{
-		if (!pathstr[i] || pathstr[i] == ':')
+		if (!pathstr[a] || pathstr[a] == ':')
 		{
-			path = dup_chars(pathstr, curr_pos, i);
+			path = dup_chars(pathstr, curr_pos, a);
 			if (!*path)
 				_strcat(path, cmd);
 			else
@@ -76,11 +76,11 @@ char *find_path(info_t *info, char *pathstr, char *cmd)
 			}
 			if (is_cmd(info, path))
 				return (path);
-			if (!pathstr[i])
+			if (!pathstr[a])
 				break;
-			curr_pos = i;
+			curr_pos = a;
 		}
-		i++;
+		a++;
 	}
 	return (NULL);
 }
