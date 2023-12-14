8bad0309bf1f6c52228d678beadc184d13944757
nothandosibanyoni <142825922+nothandosibanyoni@users.noreply.github.com>
2023-12-14 02:27:14 +0200

    Update string.c

--- string.c
+++ string.c
@@ -1,20 +1,20 @@
 #include "shell.h"
 
 /**
- * _strlen - returns the length of a string
+ * _strlen - yield the length of a string
  * @s: the string whose length to check
  *
  * Return: integer length of string
  */
 int _strlen(char *s)
 {
-	int i = 0;
+	int a = 0;
 
 	if (!s)
 		return (0);
 
 	while (*s++)
-		i++;
+		a++;
 	return (i);
 }
 
