2e4c2295e856bdc26fabb0056e422efc25b8ec7a
nothandosibanyoni <142825922+nothandosibanyoni@users.noreply.github.com>
2023-12-14 02:45:42 +0200

    Update realloc.c

--- realloc.c
+++ realloc.c
@@ -9,10 +9,10 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int a;
+	unsigned int i;
 
-	for (a = 0; a < n; a++)
-		s[a] = b;
+	for (i = 0; i < n; i++)
+		s[i] = b;
 	return (s);
 }
 
